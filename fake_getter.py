45e5abe07f7b625f392b69a21e550f9160b9bef4
gianpd <gpdomiziani@gmail.com>
2022-10-21 17:37:01 +0200

    read rgb images from bytes started.

--- fake_getter.py
+++ fake_getter.py
@@ -4,17 +4,19 @@ import time
 
 import msgpack
 
+import cv2
+import numpy as np
+
 FPS = 5
 RATE = 1 / FPS
 KEY = 'NJ'
 
 start = datetime.now()
 nj_redis = redis.Redis(host='localhost', port=6379, db=0)
-msgs = []
 for i in range(1000):
     time.sleep(RATE)
     payload = nj_redis.get(KEY)
     payload = msgpack.unpackb(payload, raw=False)
-    msgs.append(payload)
-    print(msgs[-1])
-print(f'Break: # msg {len(msgs)} - elapsed: {(datetime.now() - start).total_seconds()} [s]')
+    image = np.asarray(bytearray(payload['data']), dtype='uint8')
+    payload['data'] = cv2.cvtColor(cv2.imdecode(image, cv2.IMREAD_UNCHANGED), cv2.COLOR_BGR2RGB)
+print(f'GETTER DONE - elapsed: {(datetime.now() - start).total_seconds()} [s]') # 231 [s]
--- fake_grabber.py
+++ fake_grabber.py
@@ -1,15 +1,55 @@
+"""
+{
+"data": <bytes> #Byte array RGB ordinato per righe, colonne, canali (Height, Width, 3),
+"timestamp": <INT>, # timestamp in nanosecondi del momento di acquisizione del frame
+"size": {
+"width": <INT>, # numero colonne
+"height": <INT> # numero righe
+},
+"position": { # dati da ultima lettura del GPS, in generale antecedente il momento di acquisizione del frame
+	"timestamp": <INT>, # timestamp in nanosecondi ultima lettura del GPS
+            "longitude": <FLOAT>,
+            "latitude": <FLOAT>,
+            "altitude": <FLOAT>,
+            "track": <FLOAT>, # angolo rispetto a Nord magnetico
+            	"speed": <FLOAT>, # velocità in m/s
+"eph": <FLOAT>, # stima dell’errore di posizionamento orizzontale in metri
+"eps": <FLOAT>, # stima dell’errore di velocità orizzontale in metri/s
+"epd": <FLOAT> # stima dell’errore di track in gradi
+
+},
+"exposure": {
+	"shutter_speed": <INT>, # Velocità shutter in µs
+	"gain": <FLOAT>, # Guadagno in db, o ISO se troviamo conversione
+"aperture": <FLOAT>, # Apertura iride
+"flocal_length": <FLOAT> # focale della lente
+}
+}
+
+"""
+
 from datetime import datetime
-import json
+# import json
+from pathlib import Path
 import string
 import random
 import redis
 import time
+
+import cv2
 
 import msgpack
 
 FPS = 10
 RATE = 1 / FPS
 KEY = 'NJ'
+
+# read_img = lambda x: cv2.cvtColor(cv2.imread(x), cv2.COLOR_BGR2RGB)
+# IMG = read_img('c56d7c9f-image_2022-10-12_14-52-25.807501.jpg')
+# print(f'READ IMG: {IMG}')
+
+IMG_BYTES = Path('c56d7c9f-image_2022-10-12_14-52-25.807501.jpg').read_bytes()
+# print(f'IMG BYTES: {len(IMG_BYTES)}')
 
 grabber_redis = redis.Redis(host='localhost', port=6379, db=0)
 
@@ -21,8 +61,8 @@ for i in range(1000):
     time.sleep(RATE)
     idx = random.randint(0, N-1)
     _d = ascii_letters[idx:]
-    payload = {'d1': _d, 'd2': _d, 'd3': _d}
+    payload = {'data': IMG_BYTES, 'timestamp': 12345, 'd3': _d}
     payload = msgpack.packb(payload, use_bin_type=True)
     # grabber_redis.set(KEY, json.dumps(payload))
     grabber_redis.set(KEY, payload)
-print(f'GRABBER DONE - elapsed: {(datetime.now() - start).total_seconds()} [s]')
+print(f'GRABBER DONE - elapsed: {(datetime.now() - start).total_seconds()} [s]') # 101 [s]
